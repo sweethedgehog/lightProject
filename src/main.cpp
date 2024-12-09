defb30803fc2b8ba93be84312a03cc0038625842
kripe <kriperguy9881@mail.ru>
2024-12-09 14:21:46 +0300

    remove mode Rainbow added mode SingleColor added function calculateRainbow in class Mode changed count of setting (now up to 100)

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,12 @@ class Mode {
     public:
     Mode() {}
     virtual void setParam(String& parameters) {
-        switch (int(parameters.toInt()) % 10) {
+        switch (int(parameters.toInt()) % 100) {
             case 1:
-                brightness = parameters.toInt() / 10;
+                brightness = parameters.toInt() / 100;
                 break;
             case 2:
-                speed = parameters.toInt() / 10;
+                speed = parameters.toInt() / 100;
                 break;
         }
     }
@@ -27,107 +27,122 @@ class Mode {
         }
     }
     // рассчитывает яркость светодиода для бегущего огонька (в HSV это 3-ий параметр)
-    byte calculateRunLight(byte* i, byte* gap, byte* tailSize, bool* direction, byte& run, uint16_t* delayRunTime) {
+    byte calculateRunLight(byte* i, byte* gap, byte* tailSize, bool* direction, byte* run, uint16_t* delayRunTime) {
         if (uint32_t(millis() / int(float(delayTime) / speed * 100)) % (*delayRunTime / delayTime) == 0 && *i == 0) {
-            run++;
-            if (run > *gap)
-                run = 0;
+            (*run)++;
+            if (*run > *gap)
+                *run = 0;
         }
-        return direction ? max(0, 255 - max((*i + run) % (*gap + 1), 0) * 255 / (*tailSize + 1)) :
-            max(0, 255 - max(*gap - (*gap + *i - run) % (*gap + 1), 0) * 255 / (*tailSize + 1));
+        return direction ? max(0, 255 - max((*i + *run) % (*gap + 1), 0) * 255 / (*tailSize + 1)) :
+            max(0, 255 - max(*gap - (*gap + *i - *run) % (*gap + 1), 0) * 255 / (*tailSize + 1));
+    }
+    // рассчитывает оттенок для режимов, изпользующих радугу
+    byte calculateRainbow(byte i, byte* base, byte rainbowCount, const byte* ledCount, bool twoSides) {
+        if (i == 0)
+            (*base)++;
+        return *base + i * (rainbowCount * 255 / *ledCount) * (twoSides + 1);
     }
 
     private:
     /// настраиваемые параметры
     byte speed = 100;
-    byte brightness = 100;
+    byte brightness = 255;
     protected:
     byte delayTime;
 };
 
-class Rainbow : public Mode {
-    public:
-    Rainbow(int delayTime, int delayRunLightTime) {
+class SingleColor : public Mode {
+    public: SingleColor(int delayTime, int delayRunLightTime) {
         this->delayTime = delayTime;
         this->delayRunLightTime = delayRunLightTime;
     }
     void setParam(String& parameters) override {
         Mode::setParam(parameters);
-        switch (int(parameters.toInt() % 10)) {
+        switch (int(parameters.toInt() % 100)) {
             case 3:
-                rainbowCount = parameters.toInt() / 10;
+                shade = parameters.toInt() / 100;
                 break;
             case 4:
-                twoSides = parameters.toInt() / 10;
+                saturation = parameters.toInt() / 100;
                 break;
             case 5:
-                pastels = parameters.toInt() / 10;
+                value = parameters.toInt() / 100;
                 break;
             case 6:
-                direction = parameters.toInt() / 10;
+                gap = parameters.toInt() / 100;
                 break;
             case 7:
-                isRunningLight = parameters.toInt() / 10;
+                tailSize = parameters.toInt() / 100;
                 break;
             case 8:
-                gap = parameters.toInt() / 10;
+                rainbowCount = parameters.toInt() / 100;
                 break;
             case 9:
-                tailSize = parameters.toInt() / 10;
+                twoSides = bool(parameters.toInt() / 100);
+                break;
+            case 10:
+                direction = bool(parameters.toInt() / 100);
+                break;
+            case 11:
+                isRunningLight = bool(parameters.toInt() / 100);
+                break;
+            case 12:
+                isRainbow = bool(parameters.toInt() / 100);
                 break;
         }
     }
     void calculate(CRGB* leds, const byte* ledCount) override {
         for (byte i = 0; i < *ledCount / (twoSides + 1) + twoSides * (*ledCount % 2); i++) {
-            byte brightnessForEachLed = calculateRunLight(&i, &gap, &tailSize, &direction, run, &delayRunLightTime);
+            byte brightnessForEachLed = calculateRunLight(&i, &gap, &tailSize, &direction, &run, &delayRunLightTime);
+            byte rainbowShade = calculateRainbow(i, &base, rainbowCount, ledCount, twoSides);
             leds[direction ? i : *ledCount / (twoSides + 1) - i - 1 + twoSides] =
-                CHSV(base + i * (rainbowCount * 255 / *ledCount) * (twoSides + 1),
-                    255 - pastels, isRunningLight ? brightnessForEachLed : 255);
+                CHSV(isRainbow ? rainbowShade : shade,
+                    255 - saturation, isRunningLight ? float(brightnessForEachLed) / 255 * value : value);
             if (twoSides)
                 leds[direction ? *ledCount - i - 1 : i + *ledCount / 2] =
-                    CHSV(base + i * (rainbowCount * 255 / *ledCount) * 2,
-                        255 - pastels, isRunningLight ? brightnessForEachLed : 255);
+                    CHSV(isRainbow ? rainbowShade : shade,
+                        255 - saturation, isRunningLight ? float(brightnessForEachLed) / 255 * value : value);
         }
-        base++;
     }
-
     private:
-    byte base = 0;
     byte run = 0;
-    /// настраиваемые параметры
+    byte base = 0;
+    // настраиваемые параметры
     uint16_t delayRunLightTime;
-    byte rainbowCount = 2;
-    byte pastels = 50;
+    byte shade = 0, saturation = 0, value = 255;
     byte gap = 9;
     byte tailSize = 6;
+    byte rainbowCount = 1;
     bool twoSides = true;
     bool direction = true;
     bool isRunningLight = true;
+    bool isRainbow = true;
 };
 
 const byte NUM_LEDS = 11;
 #define PIN 12
 
 CRGB leds[NUM_LEDS];
-Mode* mode[1];
+Mode* modes[2];
+byte mode = 0;
 String receive;
 
 void setup() {
     Serial.begin(115200);
     Serial.setTimeout(100);
     FastLED.addLeds<WS2812, PIN, GRB>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-    mode[0] = new Rainbow(20, 75);
+    modes[0] = new SingleColor(20, 80);
 }
 
 void loop() {
-    mode[0]->show(leds, &NUM_LEDS);
+    modes[mode]->show(leds, &NUM_LEDS);
     delay(1);
 
     // получение с Serial порта всего необходимого
     if (Serial.available()) {
         receive += Serial.readString();
         if (receive[receive.length() - 1] == '\n') {
-            mode[0]->setParam(receive);
+            modes[mode]->setParam(receive);
             Serial.print(receive);
 
             receive = "";
